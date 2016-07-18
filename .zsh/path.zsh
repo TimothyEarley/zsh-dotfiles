3961ec415266eab1d8479442370fd108194e557e
Timothy Earley <timmy@earley.de>
2016-07-18 15:35:00 +0200

    Added scripts to path

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -4,3 +4,6 @@ export PATH=~/.dotfiles/bin/:$PATH
 # Android
 export ANDROID_HOME=${HOME}/Android/Sdk
 export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+
+# scripts
+export PATH=~/.scripts/:$PATH
