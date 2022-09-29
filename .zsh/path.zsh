4d18c359c75956a1c608c09ff0d7fc9e21215cad
Timothy Earley <timmy@earley.de>
2022-09-29 22:59:30 +0200

    Moved zsh history from ohmyzsh to custom, Moved theme config here, anon paths function

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -2,37 +2,42 @@
 export ANDROID_HOME=${HOME}/Android/Sdk
 export GRAALVM_HOME=/usr/lib/jvm/java-11-graalvm/
 
-paths=(
-	# dot(file) commands
-	~/.dotfiles/bin/
-	# Android
-	$ANDROID_HOME/tools
-	$ANDROID_HOME/platform-tools
-	# scripts
-	~/.scripts/
-	# CUDA
-	# /opt/cuda/bin
-	# Zplug failed
-	~/.zplug/bin
-	# Perl6
-	/usr/share/perl6/site/bin
-	/usr/share/perl6/vendor/bin
-	# kubectl plugins
-	~/.kube/plugins
-	# Install location for stack (Haskell)
-	~/.local/bin
-	# Jetbrains Toolbox
-	~/.local/share/JetBrains/Toolbox/scripts
-)
+function () {
 
-# why is type so slow?
-if type ruby > /dev/null; then
-	# ruby gems
-	paths+=($(ruby -e 'print Gem.user_dir')/bin)
-fi
+	local paths=(
+		# dot(file) commands
+		~/.dotfiles/bin/
+		# Android
+		$ANDROID_HOME/tools
+		$ANDROID_HOME/platform-tools
+		# scripts
+		~/.scripts/
+		# CUDA
+		# /opt/cuda/bin
+		# Zplug failed
+		~/.zplug/bin
+		# Perl6
+		/usr/share/perl6/site/bin
+		/usr/share/perl6/vendor/bin
+		# kubectl plugins
+		~/.kube/plugins
+		# Install location for stack (Haskell)
+		~/.local/bin
+		# Jetbrains Toolbox
+		~/.local/share/JetBrains/Toolbox/scripts
+	)
 
-for p in $paths
-do
-	export PATH=$PATH:$p
-done
+	# why is type so slow?
+	if type ruby > /dev/null; then
+		# ruby gems
+		paths+=(~/.local/share/gem/ruby/3.0.0/bin)
+		# hardcoded below version bc it would add ~45ms to startup
+	#	paths+=($(ruby -e 'print Gem.user_dir')/bin)
+	fi
 
+	for p in $paths
+	do
+		export PATH=$PATH:$p
+	done
+
+}
